4ddaf9921f35236b689d84e09b2c8c0fb85000da
dedegahatesyou <96036855+dedegahatesyou@users.noreply.github.com>
2022-08-29 20:03:38 -0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,9 @@
+--[[
+
+ PEDROX GUI
+ VERSION: V2.0
+ SUPPORTED GAMES: Da Hood
+
+]]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dedegahatesyou/pedrox.ggs/main/games/" .. game.PlaceId .. ".lua")()
