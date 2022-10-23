591e56003a33155c1f8463c6a8e72393c3373209
dedegahatesyou <96036855+dedegahatesyou@users.noreply.github.com>
2022-10-23 20:37:32 -0300

    Update freeanimationpack.lua

--- scripts/freeanimationpack.lua
+++ scripts/freeanimationpack.lua
@@ -1,187 +1,195 @@
-     -- // clone
-     for _, v in next, game:GetService("CoreGui"):GetChildren() do
-        if (v.Name:match("FreeAnimationPack")) then
-            v:Destroy()
-        end
-    end
+-- Gui to Lua
+-- Version: 3.2
 
-    -- // Instances
-    local FreeAnimationPack = Instance.new("ScreenGui")
-    local AnimationPack = Instance.new("TextButton")
-    local Animations = Instance.new("ScrollingFrame")
-    local UIListLayout = Instance.new("UIListLayout")
-    local Lean = Instance.new("TextButton")
-    local Lay = Instance.new("TextButton")
-    local Dance1 = Instance.new("TextButton")
-    local Dance2 = Instance.new("TextButton")
-    local Greet = Instance.new("TextButton")
-    local ChestPump = Instance.new("TextButton")
-    local Praying = Instance.new("TextButton")
-    local Stop = Instance.new("TextButton")
-    local UniversalAnimation = Instance.new("Animation")
+-- // clone
+for _, v in next, game:GetService("CoreGui"):GetChildren() do
+	if (v.Name:match("FreeAnimationPack")) then
+		v:Destroy()
+	end
+end
 
-    -- // Utility
-    function stopTracks()
-        for _, v in next, game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):GetPlayingAnimationTracks() do
-            if (v.Animation.AnimationId:match("rbxassetid")) then
-                v:Stop()
-            end
-        end
-    end
+-- Instances:
 
-    function loadAnimation(id)
-        if UniversalAnimation.AnimationId == id then
-            stopTracks()
-            UniversalAnimation.AnimationId = "1"
-        else
-            UniversalAnimation.AnimationId = id
-            local animationTrack = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):LoadAnimation(UniversalAnimation)
-            animationTrack:Play()
-        end
-    end
+local FreeAnimationPack = Instance.new("ScreenGui")
+local AnimationPack = Instance.new("TextButton")
+local Animations = Instance.new("ScrollingFrame")
+local Lean = Instance.new("TextButton")
+local Lay = Instance.new("TextButton")
+local Dance1 = Instance.new("TextButton")
+local Dance2 = Instance.new("TextButton")
+local UIListLayout = Instance.new("UIListLayout")
+local Greet = Instance.new("TextButton")
+local ChestPump = Instance.new("TextButton")
+local Praying = Instance.new("TextButton")
+local CloseButton = Instance.new("TextButton")
+local UniversalAnimation = Instance.new("Animation")
 
+-- // Utility
+function stopTracks()
+	for _, v in next, game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):GetPlayingAnimationTracks() do
+		if (v.Animation.AnimationId:match("rbxassetid")) then
+			v:Stop()
+		end
+	end
+end
 
-    FreeAnimationPack.Name = "FreeAnimationPack"
-    FreeAnimationPack.Parent = game.CoreGui
-    FreeAnimationPack.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+function loadAnimation(id)
+	if UniversalAnimation.AnimationId == id then
+		stopTracks()
+		UniversalAnimation.AnimationId = "1"
+	else
+		UniversalAnimation.AnimationId = id
+		local animationTrack = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):LoadAnimation(UniversalAnimation)
+		animationTrack:Play()
+	end
+end
 
-    AnimationPack.Name = "AnimationPack"
-    AnimationPack.Parent = FreeAnimationPack
-    AnimationPack.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    AnimationPack.BorderSizePixel = 0
-    AnimationPack.Position = UDim2.new(0, 0, 0.388007045, 0)
-    AnimationPack.Size = UDim2.new(0, 100, 0, 20)
-    AnimationPack.Font = Enum.Font.SourceSansBold
-    AnimationPack.Text = "Animations"
-    AnimationPack.TextColor3 = Color3.fromRGB(0, 0, 0)
-    AnimationPack.TextSize = 18.000
-    AnimationPack.MouseButton1Click:Connect(function()
-        if (Animations.Visible == false) then
-            Animations.Visible = true
-        end
-    end)
+--Properties:
 
-    Animations.Name = "Animations"
-    Animations.Parent = AnimationPack
-    Animations.Active = true
-    Animations.BackgroundColor3 = Color3.fromRGB(102, 102, 102)
-    Animations.Position = UDim2.new(-0.104712225, 0, -1.54173493, 0)
-    Animations.Size = UDim2.new(0, 120, 0, 195)
-    Animations.Visible = false
-    Animations.CanvasPosition = Vector2.new(0, 60.0000305)
-    Animations.CanvasSize = UDim2.new(0, 0, 1, 235)
+FreeAnimationPack.Name = "FreeAnimationPack"
+FreeAnimationPack.Parent = game.CoreGui
+FreeAnimationPack.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-    UIListLayout.Parent = Animations
-    UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-    UIListLayout.Padding = UDim.new(0, 2)
+AnimationPack.Name = "AnimationPack"
+AnimationPack.Parent = FreeAnimationPack
+AnimationPack.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+AnimationPack.BorderSizePixel = 0
+AnimationPack.Position = UDim2.new(0, 0, 0.388007045, 0)
+AnimationPack.Size = UDim2.new(0, 120, 0, 20)
+AnimationPack.Font = Enum.Font.SourceSansBold
+AnimationPack.Text = "Animations"
+AnimationPack.TextColor3 = Color3.fromRGB(0, 0, 0)
+AnimationPack.TextSize = 18.000
+AnimationPack.MouseButton1Click:Connect(function()
+	if Animations.Visible == false then
+		Animations.Visible = true
+		CloseButton.Visible = true
+		AnimationPack.BackgroundTransparency = 1
+		AnimationPack.TextTransparency = 1
+	end
+end)
 
-    Lean.Name = "Lean"
-    Lean.Parent = Animations
-    Lean.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Lean.Size = UDim2.new(1, 0, 0, 30)
-    Lean.Font = Enum.Font.SourceSansBold
-    Lean.Text = "Lean"
-    Lean.TextColor3 = Color3.fromRGB(0, 0, 0)
-    Lean.TextSize = 14.000
-    Lean.MouseButton1Click:Connect(function()
-        stopTracks()
-        loadAnimation("rbxassetid://3152375249")
-    end)
+Animations.Name = "Animations"
+Animations.Parent = AnimationPack
+Animations.Active = true
+Animations.BackgroundColor3 = Color3.fromRGB(102, 102, 102)
+Animations.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Animations.Position = UDim2.new(0, 0, 1, 0)
+Animations.Size = UDim2.new(0, 120, 0, 195)
+Animations.Visible = false
+Animations.CanvasSize = UDim2.new(0, 0, 1, 235)
+Animations.VerticalScrollBarInset = Enum.ScrollBarInset.Always
 
-    Lay.Name = "Lay"
-    Lay.Parent = Animations
-    Lay.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Lay.Size = UDim2.new(1, 0, 0, 30)
-    Lay.Font = Enum.Font.SourceSansBold
-    Lay.Text = "Lay"
-    Lay.TextColor3 = Color3.fromRGB(0, 0, 0)
-    Lay.TextSize = 14.000
-    Lay.MouseButton1Click:Connect(function()
-        stopTracks()
-        loadAnimation("rbxassetid://3152378852")
-    end)
+Lean.Name = "Lean"
+Lean.Parent = Animations
+Lean.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Lean.Size = UDim2.new(1, 0, 0, 30)
+Lean.Font = Enum.Font.GothamBlack
+Lean.Text = "Lean"
+Lean.TextColor3 = Color3.fromRGB(0, 0, 0)
+Lean.TextSize = 12.000
+Lean.MouseButton1Click:Connect(function()
+	stopTracks()
+	loadAnimation("rbxassetid://3152375249")
+end)
 
-    Dance1.Name = "Dance1"
-    Dance1.Parent = Animations
-    Dance1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Dance1.Size = UDim2.new(1, 0, 0, 30)
-    Dance1.Font = Enum.Font.SourceSansBold
-    Dance1.Text = "Dance1"
-    Dance1.TextColor3 = Color3.fromRGB(0, 0, 0)
-    Dance1.TextSize = 14.000
-    Dance1.MouseButton1Click:Connect(function()
-        stopTracks()
-        loadAnimation("rbxassetid://3189773368")
-    end)
+Lay.Name = "Lay"
+Lay.Parent = Animations
+Lay.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Lay.Size = UDim2.new(1, 0, 0, 30)
+Lay.Font = Enum.Font.GothamBlack
+Lay.Text = "Lay"
+Lay.TextColor3 = Color3.fromRGB(0, 0, 0)
+Lay.TextSize = 12.000
+Lay.MouseButton1Click:Connect(function()
+	stopTracks()
+	loadAnimation("rbxassetid://3152378852")
+end)
 
-    Dance2.Name = "Dance2"
-    Dance2.Parent = Animations
-    Dance2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Dance2.Size = UDim2.new(1, 0, 0, 30)
-    Dance2.Font = Enum.Font.SourceSansBold
-    Dance2.Text = "Dance2"
-    Dance2.TextColor3 = Color3.fromRGB(0, 0, 0)
-    Dance2.TextSize = 14.000
-    Dance2.MouseButton1Click:Connect(function()
-        stopTracks()
-        loadAnimation("rbxassetid://3189776546")
-    end)
+Dance1.Name = "Dance1"
+Dance1.Parent = Animations
+Dance1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Dance1.Size = UDim2.new(1, 0, 0, 30)
+Dance1.Font = Enum.Font.GothamBlack
+Dance1.Text = "Dance1"
+Dance1.TextColor3 = Color3.fromRGB(0, 0, 0)
+Dance1.TextSize = 12.000
+Dance1.MouseButton1Click:Connect(function()
+	stopTracks()
+	loadAnimation("rbxassetid://3189773368")
+end)
 
-    Greet.Name = "Greet"
-    Greet.Parent = Animations
-    Greet.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Greet.Size = UDim2.new(1, 0, 0, 30)
-    Greet.Font = Enum.Font.SourceSansBold
-    Greet.Text = "Greet"
-    Greet.TextColor3 = Color3.fromRGB(0, 0, 0)
-    Greet.TextSize = 14.000
-    Greet.MouseButton1Click:Connect(function()
-        stopTracks()
-        loadAnimation("rbxassetid://3189777795")
-    end)
+Dance2.Name = "Dance2"
+Dance2.Parent = Animations
+Dance2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Dance2.Size = UDim2.new(1, 0, 0, 30)
+Dance2.Font = Enum.Font.GothamBlack
+Dance2.Text = "Dance2"
+Dance2.TextColor3 = Color3.fromRGB(0, 0, 0)
+Dance2.TextSize = 12.000
+Dance2.MouseButton1Click:Connect(function()
+	stopTracks()
+	loadAnimation("rbxassetid://3189776546")
+end)
 
-    ChestPump.Name = "ChestPump"
-    ChestPump.Parent = Animations
-    ChestPump.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    ChestPump.Size = UDim2.new(1, 0, 0, 30)
-    ChestPump.Font = Enum.Font.SourceSansBold
-    ChestPump.Text = "Chest Pump"
-    ChestPump.TextColor3 = Color3.fromRGB(0, 0, 0)
-    ChestPump.TextSize = 14.000
-    ChestPump.MouseButton1Click:Connect(function()
-        stopTracks()
-        loadAnimation("rbxassetid://3189779152")
-    end)
+UIListLayout.Parent = Animations
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+UIListLayout.Padding = UDim.new(0, 2)
 
-    Praying.Name = "Praying"
-    Praying.Parent = Animations
-    Praying.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    Praying.Size = UDim2.new(1, 0, 0, 30)
-    Praying.Font = Enum.Font.SourceSansBold
-    Praying.Text = "Praying"
-    Praying.TextColor3 = Color3.fromRGB(0, 0, 0)
-    Praying.TextSize = 14.000
-    Praying.MouseButton1Click:Connect(function()
-        stopTracks()
-        loadAnimation("rbxassetid://3487719500")
-    end)
+Greet.Name = "Greet"
+Greet.Parent = Animations
+Greet.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Greet.Size = UDim2.new(1, 0, 0, 30)
+Greet.Font = Enum.Font.GothamBlack
+Greet.Text = "Greet"
+Greet.TextColor3 = Color3.fromRGB(0, 0, 0)
+Greet.TextSize = 12.000
+Greet.MouseButton1Click:Connect(function()
+	stopTracks()
+	loadAnimation("rbxassetid://3189777795")
+end)
 
-    Stop.Name = "Stop"
-    Stop.Parent = Animations
-    Stop.BackgroundColor3 = Color3.fromRGB(255, 112, 112)
-    Stop.Size = UDim2.new(1, 0, 0, 30)
-    Stop.Font = Enum.Font.SourceSansBold
-    Stop.Text = "Stop Animation"
-    Stop.TextColor3 = Color3.fromRGB(0, 0, 0)
-    Stop.TextSize = 14.000
-    Stop.MouseButton1Click:Connect(function()
-        stopTracks()
-    end)
-    --close gui
-    local plr = game.Players.LocalPlayer
+ChestPump.Name = "ChestPump"
+ChestPump.Parent = Animations
+ChestPump.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ChestPump.Size = UDim2.new(1, 0, 0, 30)
+ChestPump.Font = Enum.Font.GothamBlack
+ChestPump.Text = "Chest Pump"
+ChestPump.TextColor3 = Color3.fromRGB(0, 0, 0)
+ChestPump.TextSize = 12.000
+ChestPump.MouseButton1Click:Connect(function()
+	stopTracks()
+	loadAnimation("rbxassetid://3189779152")
+end)
 
-    plr:GetMouse().KeyDown:Connect(function(K)
-        if K == "p" then
-            Animations.Visible = false
-        end
-    end)
+Praying.Name = "Praying"
+Praying.Parent = Animations
+Praying.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Praying.Size = UDim2.new(1, 0, 0, 30)
+Praying.Font = Enum.Font.GothamBlack
+Praying.Text = "Praying"
+Praying.TextColor3 = Color3.fromRGB(0, 0, 0)
+Praying.TextSize = 12.000
+Praying.MouseButton1Click:Connect(function()
+	stopTracks()
+	loadAnimation("rbxassetid://3487719500")
+end)
+
+CloseButton.Name = "CloseButton"
+CloseButton.Parent = FreeAnimationPack
+CloseButton.BackgroundColor3 = Color3.fromRGB(255, 112, 112)
+CloseButton.Position = UDim2.new(0, 0, 0.609, 0)
+CloseButton.Size = UDim2.new(0, 120, 0, 25)
+CloseButton.Visible = false
+CloseButton.Font = Enum.Font.SourceSansBold
+CloseButton.Text = "CLOSE"
+CloseButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+CloseButton.TextSize = 30.000
+CloseButton.MouseButton1Click:Connect(function()
+	Animations.Visible = false
+	AnimationPack.BackgroundTransparency = 0
+	AnimationPack.TextTransparency = 0
+	CloseButton.Visible = false
+	stopTracks()
+end)
+
